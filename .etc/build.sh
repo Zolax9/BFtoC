dc2c3189b6d1782d3b266351fa5f811d35211bdf
LATL9 <113098926+LATL9@users.noreply.github.com>
2023-06-20 11:03:54 +0100

    fix gitignore

--- .etc/build.sh
+++
@@ -1,6 +0,0 @@
-pkill BrainfuckC-debug
-pkill BrainfuckC-release
-clear && make debug
-xdotool key Super_L+5
-#xdotool key Super_L+j
-xdotool type "cbf"
--- .etc/build2.sh
+++
@@ -1,3 +0,0 @@
-xdotool key Super_L+4
-#xdotool key Super_L+j
-xdotool type "b"
